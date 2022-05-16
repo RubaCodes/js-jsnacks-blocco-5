eec1ff6dd54788ad3d742fba28d9e87c7ce3706f
rubacodes <rubacodes@gmail.com>
2022-05-16 12:41:08 +0200

    push base snack due

---
+++ snack-2/script.js
@@ -0,0 +1,64 @@
+//array zucchine
+// Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
+// Calcola quanto pesano tutte le zucchine.
+const zucchine = [
+  {
+    tipo: 'a',
+    lunghezza: 5,
+    peso: 100,
+  },
+  {
+    tipo: 'b',
+    lunghezza: 6,
+    peso: 110,
+  },
+  {
+    tipo: 'c',
+    lunghezza: 7,
+    peso: 120,
+  },
+  {
+    tipo: 'd',
+    lunghezza: 8,
+    peso: 130,
+  },
+  {
+    tipo: 'e',
+    lunghezza: 9,
+    peso: 140,
+  },
+  {
+    tipo: 'f',
+    lunghezza: 10,
+    peso: 150,
+  },
+  {
+    tipo: 'g',
+    lunghezza: 11,
+    peso: 160,
+  },
+  {
+    tipo: 'h',
+    lunghezza: 12,
+    peso: 170,
+  },
+  {
+    tipo: 'i',
+    lunghezza: 13,
+    peso: 180,
+  },
+  {
+    tipo: 'i',
+    lunghezza: 14,
+    peso: 190,
+  },
+];
+
+let totalWeight = 0;
+for (let i = 0; i < zucchine.length; i++) {
+  totalWeight += zucchine[i].peso;
+}
+
+console.log(
+  `il peso totale  e'${totalWeight} g , ovvero ${totalWeight / 1000} kg`
+);
