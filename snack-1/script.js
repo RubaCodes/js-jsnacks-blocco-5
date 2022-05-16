113779c43c2cc5b842fbaa342c346ca8291dc284
rubacodes <rubacodes@gmail.com>
2022-05-16 12:16:00 +0200

    aggiunte 10 zucchine

---
+++ snack-1/script.js
@@ -0,0 +1,54 @@
+//array zucchine
+
+const zucchine = [
+  {
+    tipo: 'a',
+    lunghezza: 5,
+    peso: 100,
+  },
+  {
+    tipo: 'b',
+    lunghezza: 6,
+    peso: 110,
+  },
+  {
+    tipo: 'c',
+    lunghezza: 7,
+    peso: 120,
+  },
+  {
+    tipo: 'd',
+    lunghezza: 8,
+    peso: 130,
+  },
+  {
+    tipo: 'e',
+    lunghezza: 9,
+    peso: 140,
+  },
+  {
+    tipo: 'f',
+    lunghezza: 10,
+    peso: 150,
+  },
+  {
+    tipo: 'g',
+    lunghezza: 11,
+    peso: 160,
+  },
+  {
+    tipo: 'h',
+    lunghezza: 12,
+    peso: 170,
+  },
+  {
+    tipo: 'i',
+    lunghezza: 13,
+    peso: 180,
+  },
+  {
+    tipo: 'i',
+    lunghezza: 14,
+    peso: 190,
+  },
+];
