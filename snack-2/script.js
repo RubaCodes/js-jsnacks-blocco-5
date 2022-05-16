955036f79dc6d4117e63ffc80f188875e8145a68
rubacodes <rubacodes@gmail.com>
2022-05-16 12:58:42 +0200

    corretta consegna dell 'esercizio

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,6 +1,6 @@
-//array zucchine
-// Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
-// Calcola quanto pesano tutte le zucchine.
+// Crea 10 oggetti che rappresentano una zucchina.
+// Dividi in due array separati le zucchine che misurano meno o più di 15cm.
+// Infine stampa separatamente quanto pesano i due gruppi di zucchine.
 const zucchine = [
   {
     tipo: 'a',
