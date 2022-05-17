af842b487009e8c2badbb033fa826b6e3d16546d
rubacodes <rubacodes@gmail.com>
2022-05-17 12:33:02 +0200

    inserite le auto

---
+++ snack-3/script.js
@@ -0,0 +1,52 @@
+const cars = [
+  {
+    marca: 'a',
+    modello: 'aa',
+    alimetazione: 'benzina',
+  },
+  {
+    marca: 'b',
+    modello: 'bb',
+    alimetazione: 'diesel',
+  },
+  {
+    marca: 'c',
+    modello: 'cc',
+    alimetazione: 'gpl',
+  },
+  {
+    marca: 'd',
+    modello: 'dd',
+    alimetazione: 'elettrico',
+  },
+  {
+    marca: 'e',
+    modello: 'ee',
+    alimetazione: 'metano',
+  },
+  {
+    marca: 'f',
+    modello: 'ff',
+    alimetazione: 'benzina',
+  },
+  {
+    marca: 'g',
+    modello: 'gg',
+    alimetazione: 'diesel',
+  },
+  {
+    marca: 'h',
+    modello: 'hh',
+    alimetazione: 'gpl',
+  },
+  {
+    marca: 'i',
+    modello: 'ii',
+    alimetazione: 'elettrica',
+  },
+  {
+    marca: 'j',
+    modello: 'jj',
+    alimetazione: 'metano',
+  },
+];
